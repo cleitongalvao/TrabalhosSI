de139834e1b0fbca19682c98c626280e57dd6cf5
anonymous <anonymous@anonymous>
2015-03-22 21:37:45 -0300

    nova versão Labirinto

--- src/Labirinto.java
+++ src/Labirinto.java
@@ -3,22 +3,52 @@ import java.util.Scanner;
 public class Labirinto 
 {
 	private static Scanner sc;
+	private static int m[][];
+	
+	public Labirinto()
+	{
+		//sc = new Scanner(System.in);
+	}
 
 	public static void main(String[] args) 
 	{
 		sc = new Scanner(System.in);
-	    //int l;
-	    int m[][] = new int[100][30]; // matriz com 100 linhas X 30 colunas
+		lerMatriz(100,30);
+		iniciaProcuraCaminho(m, 93, 29);
+	}
+	
+	
+	private static void iniciaProcuraCaminho(int[][] Matriz, int coluna, int linha) {
+		// TODO Auto-generated method stub
 		
-		for (int i=0; i<30;i++)
+	}
+
+	public int[][] getLerMatriz(int col, int lin)
+	{
+		lerMatriz(col, lin);
+		return m;
+	}
+	
+	
+	private static void lerMatriz(int colunas, int linhas)
+	{
+	    int m[][] = new int[colunas][linhas]; // matriz com X colunas e Y linhas
+		
+		for (int i=0; i<linhas;i++)
 		{		
-			for (int j=0; j < 100;j++)
+			for (int j=0; j < colunas;j++)
 			{				
 				//l = sc.nextInt(); 
 				m[j][i] = sc.nextInt();;
 				System.out.print(m[j][i]+"");			
 			}
 			System.out.println("\n");
-		}   
+		}   		
 	}
+	
+	
+	
+	
+	
+	
 }
